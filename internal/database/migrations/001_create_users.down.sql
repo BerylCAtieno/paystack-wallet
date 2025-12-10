5750c67ee903764e10b8c61f498da661a843c38d
berylatieno <berylatieno30@gmail.com>
2025-12-10 09:24:22 +0300

    fix migration files

--- internal/database/migrations/001_create_users.down.sql
+++ internal/database/migrations/001_create_users.down.sql
@@ -1,1 +1,8 @@
+PRAGMA foreign_keys = OFF;
+
+DROP TABLE IF EXISTS api_keys;
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS wallets;
 DROP TABLE IF EXISTS users;
+
+PRAGMA foreign_keys = ON;
